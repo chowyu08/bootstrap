3fdc6ae516b3bc454c687647dacd900b28229ae3
chowyu08 <chowyu08@gmail.com>
2017-06-25 20:04:45 +0800

    'adgister'

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+   res.render('login');
+});
+router.get('/register', function(req, res) {
+   
 });
 
 module.exports = router;
