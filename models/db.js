9fcefdffb083134ac275535c92ff36ced36319a5
zhouyuyan <joy.zhou@honeywell.com>
2017-06-26 16:32:27 +0800

    ds

---
+++ models/db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+// set Promise provider to bluebird
+mongoose.Promise = require('bluebird');
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-   res.render('login');
+   res.render('register');
 });
-router.get('/register', function(req, res) {
-   
-});
+
+router.post('/register', function(req,res) {
+	// res.send(req.body);
+	// res.json({data:req.body});
+	res.render('index', { title: 'Express',name:'joy' });
+})
 
 module.exports = router;
