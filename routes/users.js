ac47a817c6cb916cd087890b3a2e1222665d3497
zhouyuyan <joy.zhou@honeywell.com>
2017-06-27 16:48:09 +0800

    user

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var userModel =require('../models/user.js')
+var userModel = require('../models/user.js')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-   res.render('register');
+	res.render('register');
 });
 
-router.post('/register', function(req,res) {
-	// res.send(req.body);
+router.post('/register', function(req, res) {
+	// console.log('username: ' + req.body.username)
+	// userModel.save()
+	var user = new userModel(req.body);
+	user.save(function(error) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('saved OK!');
+		}
+	});
+	res.send(req.body);
 	// res.json({data:req.body});
 	// res.render('index', { title: 'Express',name:'joy' });
 })
