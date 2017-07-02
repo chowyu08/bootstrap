258178b8733e09c4ed9c5899d217bcdb81dbd05a
chowyu08 <chowyu08@gmail.com>
2017-07-02 12:34:13 +0800

    'sb'

--- routes/test.js
+++ routes/test.js
@@ -4,9 +4,37 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('test', {
-		name: "test"
+		name: "dashboard"
 	});
-
 });
-
+router.get('/charts', function(req, res, next) {
+	res.render('test', {
+		name: "charts"
+	});
+});
+router.get('/tables', function(req, res, next) {
+	res.render('test', {
+		name: "table"
+	});
+});
+router.get('/forms', function(req, res, next) {
+	res.render('test', {
+		name: "forms"
+	});
+});
+router.get('/elements', function(req, res, next) {
+	res.render('test', {
+		name: "elements"
+	});
+});
+router.get('/grid', function(req, res, next) {
+	res.render('test', {
+		name: "grid"
+	});
+});
+router.get('/blank', function(req, res, next) {
+	res.render('test', {
+		name: "blank"
+	});
+});
 module.exports = router;
